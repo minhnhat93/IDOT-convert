67417fc0f1508ccc547057bf2aa3f598b1c73e19
nhat <minhnhat93@gmail.com>
2017-07-08 14:38:50 -0600

    added train and test separation

--- convert_idot_annotations_to_pascal_voc.py
+++ convert_idot_annotations_to_pascal_voc.py
@@ -6,19 +6,22 @@ from collections import OrderedDict
 import xml.etree.ElementTree as ET
 
 VIDEO_FILES = [
+  # low res
   'IDOT_dataset/videos/193402_Main_St_(US_51_Bus)_and_Empire_St_(IL_9)_in_Bloomington_20141023_11am.mp4',
   'IDOT_dataset/videos/243948_IL_126_@_Ridge_Rd._001_20150625_10am.mp4',
   'IDOT_dataset/videos/245837_FAI-74_E_of_St._Joseph_in_Champaign_County_20150630_09am.mp4',
   'IDOT_dataset/videos/251035_Princeton_34_&_26_T_20150812_08am.mp4',
   'IDOT_dataset/videos/251950_IL_8_(E.Washington_St)_&_Illini_Dr_-_Farmdale_Rd_20150818_12pm.mp4',
   'IDOT_dataset/videos/252707_FAI-74_E_of_Lincoln_Ave_in_Urbana_20150826_09am.mp4',
+  'IDOT_dataset/videos/intersection_4.avi',
+  # ----------------------------------------------
+  # high res
   'IDOT_dataset/videos/20150829_020000DST_ciceroPeterson.avi',
   'IDOT_dataset/videos/20150829_020000DST_elstonIrvingPark.avi',
   'IDOT_dataset/videos/20150918_150500DST_halsted.avi',
   'IDOT_dataset/videos/ILCHI_CHI003_20151010_075033_051.mp4',
   'IDOT_dataset/videos/ILCHI_CHI120_20151013_095039_099.mp4',
   'IDOT_dataset/videos/ILCHI_CHI164_20150930_125029_234.mp4',
-  'IDOT_dataset/videos/intersection_4.avi',
 ]
 ANNOTATION_FILES = [
   'IDOT_dataset/groundTruth/193402_Main_St_(US_51_Bus)_and_Empire_St_(IL_9)_in_Bloomington_20141023_11am.gt',
@@ -27,13 +30,14 @@ ANNOTATION_FILES = [
   'IDOT_dataset/groundTruth/251035_Princeton_34_&_26_T_20150812_08am.gt',
   'IDOT_dataset/groundTruth/251950_IL_8_(E.Washington_St)_&_Illini_Dr_-_Farmdale_Rd_20150818_12pm.gt',
   'IDOT_dataset/groundTruth/252707_FAI-74_E_of_Lincoln_Ave_in_Urbana_20150826_09am.gt',
+  'IDOT_dataset/groundTruth/intersection_4.gt',
+  # ----------------------------------------------
   'IDOT_dataset/groundTruth/20150829_020000DST_ciceroPeterson.gt',
   'IDOT_dataset/groundTruth/20150829_020000DST_elstonIrvingPark.gt',
   'IDOT_dataset/groundTruth/20150918_150500DST_halsted.gt',
   'IDOT_dataset/groundTruth/ILCHI_CHI003_20151010_075033_051.gt',
   'IDOT_dataset/groundTruth/ILCHI_CHI120_20151013_095039_099.gt',
   'IDOT_dataset/groundTruth/ILCHI_CHI164_20150930_125029_234.gt',
-  'IDOT_dataset/groundTruth/intersection_4.gt',
 ]
 
 
@@ -95,6 +99,8 @@ if __name__ == '__main__':
     pass
 
   prev_total_frame = 0
+  f_training = open('IDOT_dataset/train.txt', 'wb')
+  f_testing = open('IDOT_dataset/test.txt', 'wb')
   for video, annotation in zip(VIDEO_FILES, ANNOTATION_FILES):
     print(annotation)
     vidcap = cv2.VideoCapture(video)
@@ -107,7 +113,15 @@ if __name__ == '__main__':
       if width is None:
         width, height, channels = image.shape
     total_frames_in_this_video = frame_index - 1
+    num_training = int(float(total_frames_in_this_video) / 10.0 * 6)
+    for idx in range(total_frames_in_this_video):
+      if idx < num_training:
+        f_training.write('{}\n'.format(prev_total_frame + idx + 1).encode())
+      else:
+        f_testing.write('{}\n'.format(prev_total_frame + idx + 1).encode())
     frames_data = read_annotation(annotation, width, height, total_frames_in_this_video)
     for frame_index, frame in enumerate(frames_data):
       save_pascal_voc(join('IDOT_dataset/xml', '{}.xml'.format(prev_total_frame + frame_index + 1)), width, height, frame)
     prev_total_frame += total_frames_in_this_video
+  f_training.close()
+  f_testing.close()
---
+++ separate_train_and_test.py
@@ -0,0 +1,28 @@
+import os
+from os import mkdir
+
+
+def mkdir_if_not_exist(dn):
+  try:
+    mkdir(dn)
+  except:
+    pass
+
+
+mkdir_if_not_exist('IDOT_dataset/train')
+mkdir_if_not_exist('IDOT_dataset/train/frames')
+mkdir_if_not_exist('IDOT_dataset/train/xml')
+mkdir_if_not_exist('IDOT_dataset/test')
+mkdir_if_not_exist('IDOT_dataset/test/frames')
+mkdir_if_not_exist('IDOT_dataset/test/xml')
+
+train_set = open('IDOT_dataset/train.txt').read().splitlines()
+test_set = open('IDOT_dataset/test.txt').read().splitlines()
+
+curr_dir = os.getcwd()
+for fn in train_set:
+  os.system('ln -s {}/IDOT_dataset/frames/{}.jpg IDOT_dataset/train/frames/{}.jpg'.format(curr_dir, fn, fn))
+  os.system('ln -s {}/IDOT_dataset/xml/{}.xml IDOT_dataset/train/xml/{}.xml'.format(curr_dir, fn, fn))
+for fn in test_set:
+  os.system('ln -s {}/IDOT_dataset/frames/{}.jpg IDOT_dataset/test/frames/{}.jpg'.format(curr_dir, fn, fn))
+  os.system('ln -s {}/IDOT_dataset/xml/{}.xml IDOT_dataset/test/xml/{}.xml'.format(curr_dir, fn, fn))
